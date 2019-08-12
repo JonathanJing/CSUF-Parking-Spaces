25000aa08a00ef383353c1c5f1dbe70d2b4d9d12
Jonathan <jonathanjing@csu.fullerton.edu>
2019-08-12 15:49:53 -0700

    Update Available Spaces Web Scraping.py
    Fetch mutilble parking lot data and store in same table

--- Available Spaces Web Scraping.py
+++ Available Spaces Web Scraping.py
@@ -12,25 +12,35 @@ soup = BeautifulSoup(html, "html.parser")
 conn = sqlite3.connect('parking.sqlite')
 cur = conn.cursor()
 
-cur.execute('DROP TABLE IF EXISTS Nutwood')
+cur.execute('DROP TABLE IF EXISTS Parking')
 
 cur.execute('''
-CREATE TABLE Nutwood (available INTEGER, time TIME)''')
+CREATE TABLE Parking (
+    Name TEXT UNIQUE,
+    Available INTEGER,
+    Time TIME
+)''')
 
+class rec_data:
+  def __init__(self,building_id):
+    self.i = building_id
+  def data(self):
+    for name in soup.find_all(id='gvAvailability_HyperLink_LocationName_'+ self.i):
+      building_name = name.text
+      print(name.text)
+    for time in soup.find_all(id='gvAvailability_Label_LastUpdated_'+ self.i):
+      datetime_object = datetime.strptime(datetime_str, '%m/%d/%Y %I:%M:%S %p')
+      print(datetime_object)
+    for available in soup.find_all(id='gvAvailability_Label_Available_'+ self.i):
+      avai_num = available.text
+      print(available.text)
+    cur.execute('''INSERT INTO Parking (Name, Available, Time)
+        VALUES (?,?,?)''', (name.text, avai_num, datetime_object))
 
-for name in soup.find_all(id='gvAvailability_HyperLink_LocationName_0'):
-  print(name.text)
-for time in soup.find_all(id='gvAvailability_Label_LastUpdated_0'):
-  print(time.text)
-  datetime_str = time.text
-  datetime_object = datetime.strptime(datetime_str, '%m/%d/%Y %I:%M:%S %p')
-  print(datetime_object)
-for available in soup.find_all(id='gvAvailability_Label_Available_0'):
-  avai_num = available.text
-  print(available.text)
-
-cur.execute('''INSERT INTO Nutwood (available, time)
-        VALUES (?,?)''', (avai_num, datetime_object))
+loop = ['0','1','2']
+for i in loop:
+  p1 = rec_data(i)
+  p1.data()
 
 conn.commit()
 cur.close()
